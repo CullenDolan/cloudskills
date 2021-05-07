5facda3fc2be8a717fb9182864b0dc463c09a6ba
cullen <cullen.dolan@gmail.com>
2021-05-07 14:12:01 -0500

    adding other data types

---
+++ python-bootcamp/week2/dictionary.py
@@ -0,0 +1,13 @@
+# list
+cars = ["Grand Cherokee", "R1T", "Bronco"]
+
+print(cars[1]) # prints out 2nd item in the list
+
+# dictionary
+cars_dict = {'Make': 'Jeep', 'Model': 'Grand Cherokee', 'Year': 2017}
+print(cars_dict['Model'])
+
+# tuples
+car_tuple = ("Grand Cherokee", "R1T", "Bronco")
+
+print(type(car_tuple))
---
+++ python-bootcamp/week2/types.py
@@ -0,0 +1,10 @@
+# def subtration(five: int, ten: int):
+
+
+def add(num1: int, num2: int):
+  print(num1 + num2)
+
+num1 = 2
+num2 = 3
+add(num1, num2)
+    
